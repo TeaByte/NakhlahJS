5474ca28535412223a761992531506a9633de506
Mohammed <88824957+majhcc@users.noreply.github.com>
2024-01-15 21:50:10 +0400

    add precode for var let const

---
+++ precodes/variables/the-var-let-const.js
@@ -0,0 +1,8 @@
+const city = "Jerusalem"; // هذا المتغير لا يمكن تغيير قيمته
+// city = "Ramallah"; // TypeError: Assignment to constant variable.
+let x = 5; // هذا المتغير يمكن تغيير قيمته
+x = 6;
+console.log(x); // 6
+var y = 7; // هذا المتغير يمكن تغيير قيمته
+y = 8;
+console.log(y); // 8

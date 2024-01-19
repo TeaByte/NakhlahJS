7f07c62bba3817786c7cea1d249e50d57898bf90
teabyte <yazan4code@gmail.com>
2024-01-19 07:39:59 +0300

    fixing theme-loader.js

--- static/theme-loader.js
+++ static/theme-loader.js
@@ -1,4 +1,6 @@
 const storedTheme = localStorage.getItem("selectedTheme");
 if (storedTheme) {
   document.documentElement.setAttribute("data-theme", storedTheme);
+} else {
+  localStorage.setItem("selectedTheme", "dracula");
 }

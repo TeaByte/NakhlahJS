4d56181845ddcc20fc7ef9a35884ca93e9fb2493
Mohammed <88824957+majhcc@users.noreply.github.com>
2024-01-22 07:44:32 +0400

    Remove console.log statement in service worker

--- static/sw.js
+++ static/sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(event) {
     // TODO:
 });
 self.addEventListener('activate', function(event) {
-    console.log('Service worker activating...');
+    
 });
 self.addEventListener('fetch', function(event) {
     console.log('Fetching:', event.request.url);

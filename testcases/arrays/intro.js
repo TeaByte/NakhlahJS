7728a4f46c636ca968cbec6a047d6ad65cce0229
Mohammed <88824957+majhcc@users.noreply.github.com>
2024-02-07 21:39:02 +0400

    Add introduction to loops and example code and make quiz on intro/array

---
+++ testcases/arrays/intro.js
@@ -0,0 +1,30 @@
+// check if output is = [19, 12, 13, 14, 15];
+function handleCodeRun(code) {
+  try {
+    const capturedOutput = [];
+    const originalConsoleLog = console.log;
+    console.log = (...args) => {
+      capturedOutput.push(
+        args.map((arg) => {
+          if (typeof arg === "object" && arg !== null) {
+            return JSON.stringify(arg);
+          }
+          return arg.toString();
+        }).join(" "),
+      );
+      originalConsoleLog(...args);
+    };
+    if (code) {
+      eval(code);
+    }
+    console.log = originalConsoleLog;
+    return capturedOutput.join("\n");
+  } catch (error) {
+    return `${error}`;
+  }
+}
+const output = handleCodeRun(code);
+if (output === "19\n12\n13\n14\n15") {
+    isPass = true;
+    msg = "Pass";
+}

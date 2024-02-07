c82c426e233c4de35ac4fe14e3ac12c8f092369a
teabyte <yazan4code@gmail.com>
2024-02-07 18:09:14 +0300

    one missed lint error fixed

--- static/theme-loader.js
+++ static/theme-loader.js
@@ -5,7 +5,7 @@ if (storedTheme) {
   localStorage.setItem("selectedTheme", "dim");
 }
 
-window.dataLayer = globalThis.dataLayer || [];
+globalThis.dataLayer = globalThis.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'G-TDHJL7ZT23');

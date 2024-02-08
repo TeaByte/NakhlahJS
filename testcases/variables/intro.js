c59f91366c6d809a97120f7f34bc67ccaf3300a2
Aws / <151061059+IceBytes@users.noreply.github.com>
2024-02-08 19:53:14 +0300

    Fix testcase .

--- testcases/variables/intro.js
+++ testcases/variables/intro.js
@@ -1,8 +1,8 @@
 // check if code contain any var declaration and the value of the var is string
-const regex = /var\s+\w+\s*\=\s*\".*\"/;
+const regex = /var\s+\w+\s*(?:\=\s*\".*\")?;/;
 if (!regex.test(code)) {
     isPass = false;
-    msg = "لم يتم إنشاء متغير وتعيين قيمة له"
+    msg = "لم يتم إنشاء متغير وتعيين قيمة له";
 } else {
     isPass = true;
     msg = "اجابة صحيحة";

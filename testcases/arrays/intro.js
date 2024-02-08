d8cca6b6f17ff6fc2a982c6d3ffdc518c87983d9
Mohammed <88824957+majhcc@users.noreply.github.com>
2024-02-08 12:36:47 +0400

    remove comment in testcases/arrays/intro

--- testcases/arrays/intro.js
+++ testcases/arrays/intro.js
@@ -1,4 +1,3 @@
-// check if in the code there are array and it contains names
 const regex = /(let|const|var)\s*\w*\s*=\s*\[(("|`|')\w*\3(,|)(\s*|)(,|)(\s*|))*\]/g;
 if (regex.test(code)) {
   isPass = true;

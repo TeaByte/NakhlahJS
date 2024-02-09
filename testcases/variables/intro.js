534972bde53a1e9d0475f9695e468459501cb31f
Aws / <151061059+IceBytes@users.noreply.github.com>
2024-02-09 12:43:34 +0300

    Fix crash in variables intro.

--- testcases/variables/intro.js
+++ testcases/variables/intro.js
@@ -1,16 +1,18 @@
-// check if code contain any var declaration
-const regex = /var\s+(\w+)\s*(?:=.*)?\s*;\s*/;
+// check if code contain any var declaration 
+const regex = /var\s+(\w+)\s*(?:=\s*[^;]+)?\s*;\s*/;
 if (!regex.test(code)) {
     isPass = false;
     msg = "لم يتم إنشاء متغير وتعيين قيمة له";
 } else {
     const matches = code.match(regex);
     const varName = matches[1];
-    if (code.includes(varName + " = ")) {
+    const assignRegex = new RegExp(varName + "\\s*=\\s*[^;]+;");
+    
+    if (assignRegex.test(code)) {
         isPass = true;
         msg = "اجابة صحيحة";
     } else {
         isPass = false;
         msg = "لم يتم إنشاء متغير وتعيين قيمة له";
     }
-} 
+}

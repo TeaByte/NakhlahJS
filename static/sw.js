c794f31ac8939b903970652712d680cea46f13e8
Mohammed <1mohammed.bin.saif@gmail.com>
2024-02-17 22:07:09 +0400

    Refactor service worker code for caching and offline support

--- static/sw.js
+++ static/sw.js
@@ -1,68 +1,89 @@
-// const CACHE_NAME = 'cache-v1';
-// const no_cache_urls = [
-//   "/sw-cache.json",
-// ]
-// self.addEventListener('install', function (event) {
-//   event.waitUntil(
-//     () => {
-//       const cacheNames = caches.keys();
-//       Promise.all(
-//         cacheNames.map(function (cacheName) {
-//           if (cacheName !== CACHE_NAME) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//       cacheNewResources()
-//     }
-//   );
-// });
+const CACHE_NAME = 'cache-v1';
+const no_cache_urls = [
+    "/sw-cache.json",
+]
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        () => {
+            const cacheNames = caches.keys();
+            Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+            cacheNewResources()
+        }
+    );
+});
 
-// self.addEventListener('activate', function (event) {
-//   event.waitUntil(
-//     caches.keys().then(function (cacheNames) {
-//       return Promise.all(
-//         cacheNames.map(function (cacheName) {
-//           if (cacheName !== CACHE_NAME) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     }).then(function () {
-//       return cacheNewResources();
-//     })
-//   );
-// });
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(function () {
+            return cacheNewResources();
+        })
+    );
+});
 
-// async function cacheNewResources() {
-//   const resp = await fetch('/sw-cache.json');
-//   const data = await resp.json();
-//   const cache = await caches.open(CACHE_NAME);
-//   await cache.addAll([...data, "/", "/offline", "/courses"]);
-// }
+async function cacheNewResources() {
+    const resp = await fetch('/sw-cache.json');
+    const data = await resp.json();
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll([...data, "/", "/offline", "/courses"]);
+}
 
-// self.addEventListener('fetch', function (event) {
-//   const url = new URL(event.request.url).pathname
-//   if (no_cache_urls.includes(url)) {
-//     event.respondWith(fetch(event.request));
-//   } else {
-//     event.respondWith(
-//       caches.match(event.request).then(function (response) {
-//         if (response) {
-//           return response;
-//         } else {
-//           if (event.request.method === 'GET') {
-//             return fetch(event.request).then(function (response) {
-//               return caches.open(CACHE_NAME).then(function (cache) {
-//                 cache.put(event.request, response.clone());
-//                 return response;
-//               });
-//             });
-//           } else {
-//             return fetch(event.request);
-//           }
-//         }
-//       })
-//     );
-//   }
-// });
+self.addEventListener('fetch', function (event) {
+    const url = new URL(event.request.url).pathname
+    // ! this code block is for offline support temproray
+    // TODO: remove this code block
+    if (!navigator.onLine) {
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else {
+                    return caches.match(new Request('/offline'));
+                }
+            })
+        );
+    }
+    // ! this code update cache on every request it is not good for performance, but it make app works offline
+    // TODO: remove this code block
+    if (event.request.method === 'GET' && event.request.url.origin === location.origin) {
+        const cache = await caches.open(CACHE_NAME);
+        cache.add(event.request.url);
+        return fetch(event.request);
+    }
+    // TODO: Implement cache strategy
+    //   if (no_cache_urls.includes(url)) {
+    //     event.respondWith(fetch(event.request));
+    //   } else {
+    //     event.respondWith(
+    //       caches.match(event.request).then(function (response) {
+    //         if (response) {
+    //           return response;
+    //         } else {
+    //           if (event.request.method === 'GET') {
+    //             return fetch(event.request).then(function (response) {
+    //               return caches.open(CACHE_NAME).then(function (cache) {
+    //                 cache.put(event.request, response.clone());
+    //                 return response;
+    //               });
+    //             });
+    //           } else {
+    //             return fetch(event.request);
+    //           }
+    //         }
+    //       })
+    //     );
+    //   }
+});

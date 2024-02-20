d9825c98dd232c61c55c7e23944de82f2e2e63c2
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-02-20 20:03:50 +0400

    Add partial components for Lessons

--- components/MarkdownSplit.tsx
+++ components/MarkdownSplit.tsx
@@ -5,6 +5,7 @@ import EditButton from "../components/EditButton.tsx";
 
 import IconPlayerTrackNext from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/player-track-next.tsx";
 import IconPlayerTrackPrev from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/player-track-prev.tsx";
+import { Partial } from "$fresh/runtime.ts";
 
 interface MarkdownSplitProps {
   course: Course;
@@ -24,63 +25,70 @@ export default function MarkdownSplit({
   return (
     <>
       <div class="flex px-2 pt-2 gap-2">
-        {nextCourse && (
-          <div class="grow">
-            <a
-              href={`/${nextCourse}`}
-              class="flex items-center gap-2 btn btn-outline flex-nowrap"
-            >
-              <IconPlayerTrackPrev size={18} />
-              <span>الدرس التالي</span>
-            </a>
-          </div>
-        )}
+        <Partial name="navigation-buttons">
+          {nextCourse && (
+            <div class="grow">
+              <a
+                href={`/${nextCourse}`}
+                class="flex items-center gap-2 btn btn-outline flex-nowrap"
+              >
+                <IconPlayerTrackPrev size={18} />
+                <span>الدرس التالي</span>
+              </a>
+            </div>
+          )}
 
-        {prevCourse && (
-          <div class="grow">
-            <a
-              href={`/${prevCourse}`}
-              class="flex items-center gap-2 btn btn-outline flex-nowrap"
-            >
-              <span>الدرس السابق</span>
-              <IconPlayerTrackNext size={18} />
-            </a>
-          </div>
-        )}
+          {prevCourse && (
+            <div class="grow">
+              <a
+                href={`/${prevCourse}`}
+                class="flex items-center gap-2 btn btn-outline flex-nowrap"
+              >
+                <span>الدرس السابق</span>
+                <IconPlayerTrackNext size={18} />
+              </a>
+            </div>
+          )}
+        </Partial>
       </div>
       <section dir="rtl" class="p-3 mb-40 md:mb-5">
         <div>
           <div className="flex flex-col-reverse">
             <div class="flex flex-col-reverse text-sm breadcrumbs" dir="rtl">
-              <ul>
-                <li>
-                  <a href="/courses">الدروس</a>
-                </li>
-                {lable && (
+              <Partial name="breadcrumbs">
+                <ul>
                   <li>
-                    <a href={`/group/${lableSlug}`}>{lable}</a>
+                    <a href="/courses">الدروس</a>
                   </li>
-                )}
-                <li class="underline">{course.title}</li>
-              </ul>
+                  {lable && (
+                    <li>
+                      <a href={`/group/${lableSlug}`}>{lable}</a>
+                    </li>
+                  )}
+                  <li class="underline">{course.title}</li>
+                </ul>
+              </Partial>
             </div>
           </div>
           <div class="flex flex-col gap-2 md:flex-row justify-between mb-4">
-            <h1 class="text-3xl">{course.title}</h1>
-            <EditButton slug={course.slug} />
+            <Partial name="course-header">
+              <h1 class="text-3xl">{course.title}</h1>
+              <EditButton slug={course.slug} />
+            </Partial>
           </div>
         </div>
-
-        <div
-          id="document"
-          class="markdown-body"
-          style={{ backgroundColor: "inherit" }}
-          dangerouslySetInnerHTML={{
-            __html: render(course.content, {
-              disableHtmlSanitization: true,
-            }),
-          }}
-        />
+        <Partial name="course-content">
+          <div
+            id="document"
+            class="markdown-body"
+            style={{ backgroundColor: "inherit" }}
+            dangerouslySetInnerHTML={{
+              __html: render(course.content, {
+                disableHtmlSanitization: true,
+              }),
+            }}
+          />
+        </Partial>
       </section>
     </>
   );

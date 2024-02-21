d7a3757ef7c00db532fb5aba75b45fcf9212af67
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-02-21 15:18:10 +0400

    AddQuiz for multidimensional array

---
+++ testcases/arrays/multidimensional-index.js
@@ -0,0 +1,8 @@
+const regex = /console.log\((\s+|)+arr\[3\]\[1\](\s+|)\)/
+if (regex.test(code)) {
+    isPass = true
+    msg = "مبروك، الجواب صحيح"
+} else {
+    isPass = false
+    msg = "الجواب خاطئ"
+}

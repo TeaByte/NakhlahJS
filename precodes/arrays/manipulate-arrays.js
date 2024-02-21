f1ba9b8e1b18e1b52b7e1ac0bf4261ded63b0097
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-02-21 15:40:14 +0400

    ADD to manipulate-arrays.md a  quiz question

---
+++ precodes/arrays/manipulate-arrays.js
@@ -0,0 +1,1 @@
+const myArray = ["a", "b", "c", "d"];
---
+++ testcases/arrays/manipulate-arrays.js
@@ -0,0 +1,33 @@
+function handleCodeRun(code) {
+  try {
+    const capturedOutput = [];
+    const originalConsoleLog = console.log;
+    console.log = (...args) => {
+      capturedOutput.push(
+        args.map((arg) => {
+          if (typeof arg === "object" && arg !== null) {
+            return JSON.stringify(arg);
+          }
+          return arg.toString();
+        }).join(" "),
+      );
+      originalConsoleLog(...args);
+    };
+    if (code) {
+      eval(code);
+    }
+    console.log = originalConsoleLog;
+    return capturedOutput.join("\n");
+  } catch (error) {
+    return `${error}`;
+  }
+}
+code += "\nconsole.log(myArray);";
+const out = handleCodeRun(code);
+if (out === '["b","c","d","Ali",35]') {
+  isPass = true;
+  msg = "Pass!";
+} else {
+    isPass = false;
+    msg = "Fail!";
+}

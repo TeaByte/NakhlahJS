e9cf75f42dc88ea19691b7952e36f42b3961e947
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-02-26 01:09:17 +0400

    Make testcases for all lessons #10

---
+++ precodes/objects/delete-properties-from-object.js
@@ -0,0 +1,9 @@
+const fighter = {
+    name: 'Mig 29',
+    country: 'Egypt',
+    speed: 2.25,
+    speedUnit: 'Mach',
+    maxAltitude: 65000,
+    maxAltitudeUnit: 'ft',
+    operator: 'Egyptian Air Force',
+}
---
+++ testcases/objects/delete-properties-from-object.js
@@ -0,0 +1,7 @@
+// if code contain `delete fighter.operator` then isPass = true
+if (code.includes("delete fighter.operator")) {
+    isPass = true;
+} else {
+    isPass = false;
+    msg = "الله يبارك فيك ركز شوية وحاول مره ثانية"
+}

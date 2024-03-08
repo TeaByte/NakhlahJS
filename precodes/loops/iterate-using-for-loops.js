6bf5954d67862b492914024672568c8bc0211323
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-03-08 15:57:00 +0400

    Tests that need adjustments #33

---
+++ precodes/loops/iterate-using-for-loops.js
@@ -0,0 +1,2 @@
+// the varible oddNumbers must be an array of odd numbers from 1 to 10 [1, 3, 5, 7, 9];
+var oddNumbers = [];
---
+++ testcases/loops/iterate-using-for-loops.js
@@ -0,0 +1,36 @@
+function handleCodeRun(code) {
+    try {
+        const capturedOutput = [];
+        const originalConsoleLog = console.log;
+        console.log = (...args) => {
+            capturedOutput.push(
+                args.map((arg) => {
+                    if (typeof arg === "object" && arg !== null) {
+                        return JSON.stringify(arg);
+                    }
+                    return arg.toString();
+                }).join(" "),);
+            originalConsoleLog(...args);
+        };
+        if (code) {
+            eval(code);
+        }
+        console.log = originalConsoleLog;
+        return capturedOutput
+    } catch (error) {
+        return `${error}`;
+    }
+}
+if (code.includes("for")) {
+    let output = handleCodeRun(code+"\nconsole.log(oddNumbers);")
+    output = output[output.length - 1];
+    if (output == JSON.stringify([1, 3, 5, 7, 9])) {
+        isPass = true;
+    } else {
+        isPass = false;
+        msg = "تأكد من النتائج";
+    }
+} else {
+    isPass = false;
+    msg = "الرجاء التأكد من استخدام الحلقة الصحيحة للتكرار.";
+}
